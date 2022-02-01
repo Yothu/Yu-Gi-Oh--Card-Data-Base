af676df2f72fd5025a9cfaa79cb2f14911b798d6
David Vergaray <david.vergaray.a@gmail.com>
2022-01-31 23:22:39 -0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import './style.css';
 import './font-style.css';
 import loadImage from './load-image-module.js';
 
-
 import logo from './components/images/monster-cards-logo.png';
-const headerImageContainer = document.querySelector('.header-logo-container');
 
+const headerImageContainer = document.querySelector('.header-logo-container');
 
 const headerLogo = loadImage(logo, headerImageContainer, 'logo');
 headerLogo.classList.add('header-logo');

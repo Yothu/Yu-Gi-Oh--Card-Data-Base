991b7ed544da9df36cb88ebc69c977d0146d4aa1
David Vergaray <david.vergaray.a@gmail.com>
2022-02-03 13:44:03 -0300

    Finish comment counter tests

--- src/__test__/commentCounter.test.js
+++ src/__test__/commentCounter.test.js
@@ -5,7 +5,7 @@
 import popupCommentCounter from '../commentCounter.js';
 
 describe('Comment counter function tests', () => {
-  test('If there are no comment-elements inside the comment container, the function should add "Comments (0)" inside the comment title', () => {
+  test('There are no comment-elements inside the comment container, the function should add "Comments (0)" in the comment title', () => {
     // ARRANGE
     document.body.innerHTML = `
     <div class="comments-container-title"></div>
@@ -19,15 +19,47 @@ describe('Comment counter function tests', () => {
     expect(commentTitle).toBe('Comments (0)');
   });
 
-  test('should ', () => {
+  test('There are six comment-elements inside the comment container, the function should add "Comments (6)" in the comment title', () => {
     // ARRANGE
+    document.body.innerHTML = `
+    <div class="comments-container-title"></div>
+    <div class="comment-container">
+      <div class="comment-element"></div>
+      <div class="comment-element"></div>
+      <div class="comment-element"></div>
+      <div class="comment-element"></div>
+      <div class="comment-element"></div>
+      <div class="comment-element"></div>
+    </div>
+    `;
+    const cardContainer = document.querySelector('.comment-container');
     // ACT
+    popupCommentCounter(cardContainer);
+    const commentTitle = document.querySelector('.comments-container-title').textContent;
     // ASSERT
+    expect(commentTitle).toBe('Comments (6)');
   });
 
-  test('should ', () => {
+  test('There are 3 direct children inside the comment container that are comment-element, the function should ignore indirect children and add "Comments (3)" in the comment title', () => {
     // ARRANGE
+    document.body.innerHTML = `
+    <div class="comments-container-title"></div>
+    <div class="comment-container">
+      <div class="comment-element"></div>
+      <div class="comment-element">
+        <div class="comment-element"></div>
+      </div>
+      <p class="paragraph"></p>
+      <span class="spank"></span>
+      <div class="comment-element"></div>
+      <hr>
+    </div>
+    `;
+    const cardContainer = document.querySelector('.comment-container');
     // ACT
+    popupCommentCounter(cardContainer);
+    const commentTitle = document.querySelector('.comments-container-title').textContent;
     // ASSERT
+    expect(commentTitle).toBe('Comments (3)');
   });
 });
